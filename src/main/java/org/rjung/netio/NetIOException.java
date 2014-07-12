91350e56dc9feb6029d4956221ea09cad0f719d9
Rainer Jung <Rainer.Jung@gmail.com>
2014-07-12 11:14:32 +0200

    Added constructor for NetIOException

--- src/main/java/org/rjung/netio/NetIOException.java
+++ src/main/java/org/rjung/netio/NetIOException.java
@@ -1,17 +1,18 @@
 package org.rjung.netio;
 
-
 public class NetIOException extends Exception {
 
-    public NetIOException(String message) {
-        super(message);
-    }
+	public NetIOException(String message) {
+		super(message);
+	}
 
+	public NetIOException(Throwable e) {
+		super(e);
+	}
 
-    public NetIOException(Throwable e) {
-        super(e);
-    }
+	public NetIOException(String message, Throwable e) {
+		super(message, e);
+	}
 
-
-    private static final long serialVersionUID = -1948688832753938552L;
+	private static final long serialVersionUID = -1948688832753938552L;
 }
